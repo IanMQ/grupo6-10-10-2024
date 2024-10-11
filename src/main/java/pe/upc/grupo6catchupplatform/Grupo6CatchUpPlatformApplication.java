ee64f3d0c9288000b6ad36f1e6079a528482680a
IanMQ <129208479+IanMQ@users.noreply.github.com>
2024-10-10 20:32:04 -0500

    initial commit

---
+++ src/main/java/pe/upc/grupo6catchupplatform/Grupo6CatchUpPlatformApplication.java
@@ -0,0 +1,13 @@
+package pe.upc.grupo6catchupplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo6CatchUpPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Grupo6CatchUpPlatformApplication.class, args);
+    }
+
+}

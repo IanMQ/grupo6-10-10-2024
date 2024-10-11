f20cd7bdc939ceace32560797134ff4d474d331b
IanMQ <129208479+IanMQ@users.noreply.github.com>
2024-10-10 21:21:18 -0500

    feat: new FavoriteSource

--- src/main/java/pe/upc/grupo6catchupplatform/news/domain/model/aggregates/FavoriteSource.java
+++ src/main/java/pe/upc/grupo6catchupplatform/news/domain/model/aggregates/FavoriteSource.java
@@ -5,5 +5,11 @@ import org.springframework.data.domain.AbstractAggregateRoot;
 public class FavoriteSource extends AbstractAggregateRoot<FavoriteSource> {
 
     public Long id;
+    public String name;
+    public String description;
+    public String url;
+    public String category;
+    public String language;
+    public String country;
 
 }
